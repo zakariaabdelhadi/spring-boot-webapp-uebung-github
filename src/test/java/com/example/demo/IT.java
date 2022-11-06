ac2d71c1486a680fd26c1e87b1d862e4fdccbe46
zakaria <zakizermani@gmail.com>
2022-11-06 03:58:10 +0100

    build automation

---
+++ src/test/java/com/example/demo/IT.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IT {
+
+	@Test
+void my_simple_integration_test() {
+System.out.println("This is a integration test!");
+}
+
+}
